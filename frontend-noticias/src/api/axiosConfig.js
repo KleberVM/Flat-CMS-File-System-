314176f4ae5cc2a9f271b991b20faf5675f9274f
Kleber <muruchi2018@gmail.com>
2026-03-03 07:43:40 -0400

    cambiando url de api

--- frontend-noticias/src/api/axiosConfig.js
+++ frontend-noticias/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const clienteAxios = axios.create({
-    baseURL: 'http://localhost:3000/api' 
+    baseURL: 'https://flat-cms-ar1z.onrender.com/api' 
 });
 
 export default clienteAxios;
